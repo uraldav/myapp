eddd8d940d2fedf9fc9541c5ec221bc59a7a4075
Vyacheslav B <la.sintez@gmail.com>
2017-09-05 18:28:42 +0300

    feat: sidemenu, rendered with arrays, not finished yet

---
+++ app/components/App/SideMenu.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import { object } from 'prop-types';
+import { compose, pure } from 'recompose';
+import { Menu, Icon } from 'antd';
+import FaIcon from 'react-fontawesome';
+import queryString from 'query-string';
+import MenuLink from './MenuLink';
+import './SideMenu.less';
+
+const { SubMenu } = Menu;
+
+SideMenu.propTypes = {
+  location: object.isRequired,
+};
+
+function SideMenu({ location }) {
+  const queryParams = queryString.parse(location.search);
+
+  return (
+    <Menu
+      multiple
+      mode="inline"
+      styleName="menu"
+      selectedKeys={[queryParams.socialId]}
+    >
+      <SubMenu
+        key="twitter"
+        className={queryParams.socialId === 'twitter' ? 'selected' : ''}
+        title={
+          <MenuLink
+            title="Twitter"
+            icon={<FaIcon name="twitter" />}
+            to={{
+              pathname: '/',
+              search: `?${queryString.stringify({
+                ...queryParams,
+                socialId: 'twitter',
+              })}`,
+            }}
+            badge={9}
+          />
+        }
+      >
+        {[
+          { title: 'Чубайс', id: 'chubais', badge: 7 },
+          { title: 'Пушкин', id: 'pooshka', badge: 4 },
+          { title: 'Ленин', id: 'lenin', badge: 1 },
+        ].map(menuItem => (
+          <Menu.Item key={menuItem.id}>
+            <MenuLink
+              title={menuItem.title}
+              to={{
+                pathname: '/',
+                search: `?${queryString.stringify({
+                  ...queryParams,
+                  socialId: menuItem.id,
+                })}`,
+              }}
+              badge={menuItem.badge}
+            />
+          </Menu.Item>
+        ))}
+      </SubMenu>
+      <SubMenu
+        key="facebook"
+        className={queryParams.socialId === 'facebook' ? 'selected' : ''}
+        title={
+          <MenuLink
+            title="Facebook"
+            icon={<FaIcon name="facebook-square" />}
+            to={{
+              pathname: '/',
+              search: `?${queryString.stringify({
+                ...queryParams,
+                socialId: 'facebook',
+              })}`,
+            }}
+            badge={9}
+          />
+        }
+      >
+        {[
+          { title: 'Чубайс Фейс', id: 'f-chubais', badge: 7 },
+          { title: 'Пушкин Фейс', id: 'f-pooshka', badge: 4 },
+          { title: 'Ленин Фейс', id: 'f-lenin', badge: 1 },
+        ].map(menuItem => (
+          <Menu.Item key={menuItem.id}>
+            <MenuLink
+              title={menuItem.title}
+              to={{
+                pathname: '/',
+                search: `?${queryString.stringify({
+                  ...queryParams,
+                  socialId: menuItem.id,
+                })}`,
+              }}
+              badge={menuItem.badge}
+            />
+          </Menu.Item>
+        ))}
+      </SubMenu>
+      <SubMenu
+        key="others"
+        title={
+          <span>
+            <FaIcon name="square-o" />
+            <span>Неявные упоминания</span>
+          </span>
+        }
+      >
+        <Menu.Item>
+          <span>аэрофлот</span>
+        </Menu.Item>
+      </SubMenu>
+      <SubMenu
+        key="settings"
+        title={
+          <span>
+            <Icon type="setting" />
+            <span>Справочники и настройки</span>
+          </span>
+        }
+      >
+        <Menu.Item>
+          <Icon type="team" />
+          <span>Пользователи</span>
+        </Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
+}
+
+export default compose(pure)(SideMenu);
