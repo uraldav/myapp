8245493d5f963e9990afeea9ca03b347bd3e0bf7
Anton Zalaldinov <zalaldinov@icloud.com>
2017-09-07 16:52:47 +0300

    made some new sagas

--- app/containers/Users/sagas.js
+++ app/containers/Users/sagas.js
@@ -2,17 +2,24 @@ import { call, cancel, fork, getContext, put, take, takeLatest } from 'redux-sag
 import { LOCATION_CHANGE } from 'react-router-redux';
 import {
   REQUEST,
+  DELETE_REQUEST,
+  CREATE_REQUEST,
   requestSuccess,
   requestFailure,
+  deleteSuccess,
+  deleteFailure,
+  createSuccess,
+  createFailure,
 } from './ducks';
 
 export default function* () {
   const watchRequest = yield takeLatest(REQUEST, requestSaga);
-
+  const watchDeleteRequest = yield takeLatest(DELETE_REQUEST, deleteUserSaga);
+  const watchCreateRequest = yield takeLatest(CREATE_REQUEST, deleteUserSaga);
   yield fork(requestSaga);
 
   yield take(LOCATION_CHANGE);
-  yield cancel(watchRequest);
+  yield cancel(watchRequest, watchDeleteRequest, watchCreateRequest);
 }
 
 export function* requestSaga() {
@@ -23,5 +30,26 @@ export function* requestSaga() {
     yield put(requestSuccess(response));
   } catch (error) {
     yield put(requestFailure(error));
+  }
+}
+
+
+export function* deleteUserSaga() {
+  const api = yield getContext('api');
+  try {
+    const response = yield call(api.users.deleteUsers);
+    yield put(deleteSuccess(response));
+  } catch (error) {
+    yield put(deleteFailure(error));
+  }
+}
+
+export function* createUserSaga() {
+  const api = yield getContext('api');
+  try {
+    const response = yield call(api.users.createUser);
+    yield put(createSuccess(response));
+  } catch (error) {
+    yield put(createFailure);
   }
 }
--- app/services/api/users.js
+++ app/services/api/users.js
@@ -3,3 +3,13 @@ export const fetchUsers = () =>
   fetch('/api/users',
     { method: 'GET' })
     .then(response => response.json());
+
+export const deleteUser = () =>
+  fetch('/api/users',
+  { method: 'DELETE' })
+  .then(response => response.json());
+
+export const createUser = () =>
+  fetch('/api/users',
+  { method: 'POST' })
+  .then(response => response.json());
