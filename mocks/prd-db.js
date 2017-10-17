1e363a33c9a43b2aa681476df8ae7fd8a99efdf4
Vyacheslav B <la.sintez@gmail.com>
2017-10-17 17:36:58 +0300

    fix: fixed mocks, so user-roles work now on investigation

--- mocks/prd-db.js
+++ mocks/prd-db.js
@@ -7,7 +7,7 @@ module.exports = () => {
     users: require('./prd-mock/users')(),
     input_thematics: require('./prd-mock/input_thematics')(),
     model_thematics: require('./prd-mock/model_thematics')(),
-    user_roles: require('./user_roles')(),
+    user_roles: require('./prd-mock/user_roles')(),
     permissions: require('./prd-mock/permissions')(),
     priority_coefficients: require('./prd-mock/priority_coefficients')(),
     important_authors: require('./prd-mock/important_authors')(),
